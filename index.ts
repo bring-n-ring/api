acdb0bd62c629337cdd887e3e040ce7c0f9bdedc
Xavier Torremade Barreda <xavier.torremade@flixbus.com>
2020-03-26 22:35:55 +0100

    Connected to firestore + added firestorm

--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
-import * as functions from 'firebase-functions';
+import * as firestorm from 'firebase-firestorm';
 import { AppModule } from './src/app.module';
-import admin = require('firebase-admin');
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
 
 const expressServer = express();
 
 const createFunction = async (expressInstance): Promise<void> => {
+  admin.initializeApp(functions.config().firebase);
+  const firestore = admin.firestore();
+  firestorm.initialize(firestore);
+
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressInstance),
